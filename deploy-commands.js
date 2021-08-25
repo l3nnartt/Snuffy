3a79b428e92abdef9cb35efce8aa76332222bd28
l3nnartt <loesche.lennart@gmail.com>
2021-08-25 05:34:52 +0200

    Add Command Handler

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,9 +4,14 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('userinfo').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('avatar').setDescription('Avatar'),
+	new SlashCommandBuilder().setName('beep').setDescription('beep'),
+	new SlashCommandBuilder().setName('kick').setDescription('kick'),
+	new SlashCommandBuilder().setName('options-info').setDescription('options-info'),
+	new SlashCommandBuilder().setName('ping').setDescription('ping'),
+	new SlashCommandBuilder().setName('prune').setDescription('prune'),
+	new SlashCommandBuilder().setName('servers').setDescription('servers'),
+	new SlashCommandBuilder().setName('user-info').setDescription('user-info'),
 ]
 	.map(command => command.toJSON());
 
