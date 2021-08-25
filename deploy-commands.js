7dbdef3fe6374ec92de371d145a4f744a7d7a605
l3nnartt <loesche.lennart@gmail.com>
2021-08-25 05:42:58 +0200

    Dynamic register / commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,15 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [
-	new SlashCommandBuilder().setName('avatar').setDescription('Avatar'),
-	new SlashCommandBuilder().setName('beep').setDescription('beep'),
-	new SlashCommandBuilder().setName('kick').setDescription('kick'),
-	new SlashCommandBuilder().setName('options-info').setDescription('options-info'),
-	new SlashCommandBuilder().setName('ping').setDescription('ping'),
-	new SlashCommandBuilder().setName('prune').setDescription('prune'),
-	new SlashCommandBuilder().setName('servers').setDescription('servers'),
-	new SlashCommandBuilder().setName('user-info').setDescription('user-info'),
-]
-	.map(command => command.toJSON());
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '9' }).setToken(token);
 
