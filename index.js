7dbb1e816c73c65ccb591fd6d0714c32ac1f26ac
l3nnartt <loesche.lennart@gmail.com>
2021-08-25 05:26:57 +0200

    Add Slash Commands

--- index.js
+++ index.js
@@ -7,4 +7,18 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'userinfo') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.login(token);
