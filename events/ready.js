725de0a73f94e02aeda0826b5b818e556c0eba31
l3nnartt <loesche.lennart@gmail.com>
2021-08-25 20:34:38 +0200

    Rich Presence

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,16 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		setInterval(() => {
+			const activities = [
+				`github.com/l3nartt/snuffy`,
+				`${client.guilds.cache.reduce((a, g) => a + g.memberCount,0)} Mitglieder`
+			];
+			let activity = activities[Math.floor(Math.random() * activities.length)];
+			client.user.setActivity(activity,
+				{type: "PLAYING"}
+			);
+  		},15000);
 	},
 };
