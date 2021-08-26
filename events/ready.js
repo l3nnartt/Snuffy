9cfa99b2284abea55c9aec23b8c87dc9349fc6cf
l3nnartt <loesche.lennart@gmail.com>
2021-08-26 02:21:50 +0200

    Minify RPC

--- events/ready.js
+++ events/ready.js
@@ -5,11 +5,7 @@ module.exports = {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 
 		setInterval(() => {
-			const activities = [
-				`github.com/l3nartt/snuffy`,
-				`${client.guilds.cache.reduce((a, g) => a + g.memberCount,0)} Mitglieder`
-			];
-			let activity = activities[Math.floor(Math.random() * activities.length)];
+			let activity = `${client.guilds.cache.reduce((a, g) => a + g.memberCount,0)} Mitglieder`;
 			client.user.setActivity(activity,
 				{type: "PLAYING"}
 			);
