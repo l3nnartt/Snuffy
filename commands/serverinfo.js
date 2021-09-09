60d9d71266c3a17e77061e4931fb4f780f7cfbcb
l3nnartt <loesche.lennart@gmail.com>
2021-09-09 18:30:14 +0200

    Update Serverinfo Command
    Add Lifetime Member Count

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,21 +1,32 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const { member } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('serverinfo')
 		.setDescription('Display info about this server.'),
 	async execute(interaction, client) {
-		const reply = new MessageEmbed()
-			.setTitle(`${client.user.username} • Serverinfo`)
-			.addFields(
-				{ name: 'Servername:', value: `${interaction.guild.name}` },
-				{ name: 'Current members:', value: `${interaction.guild.memberCount}` },
-				{ name: 'Members alltime', value: `COMING SOON` },
-				{ name: 'Server created:', value: `${new Date(interaction.guild.createdTimestamp).toLocaleDateString()} ${new Date(interaction.guild.createdTimestamp).toLocaleTimeString()}` }
-			)
-			.setTimestamp(interaction.createdAt)
-			.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
-		return interaction.reply({embeds: [reply]});
+		fetch(member, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+			const reply = new MessageEmbed()
+				.setTitle(`${client.user.username} • Serverinfo`)
+				.addFields(
+					{ name: 'Servername:', value: `${interaction.guild.name}` },
+					{ name: 'Current members:', value: `${interaction.guild.memberCount}` },
+					{ name: 'Members alltime', value: `${data.member}` },
+					{ name: 'Server created:', value: `${new Date(interaction.guild.createdTimestamp).toLocaleDateString()} ${new Date(interaction.guild.createdTimestamp).toLocaleTimeString()}` }
+				)
+				.setTimestamp(interaction.createdAt)
+				.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+			return interaction.reply({embeds: [reply]});
+		})
 	},
 };
