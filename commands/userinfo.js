f482a34aa762195f6f68f490ec16169575f210c5
l3nnartt <loesche.lennart@gmail.com>
2021-09-09 18:27:10 +0200

    Update Userinfo Command
    Add ID to the Userinfo Command

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const { api } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,25 +11,34 @@ module.exports = {
 	async execute(interaction, client) {
 		const user = interaction.options.getUser('target') || interaction.user;
 		const member = interaction.guild.members.cache.get(user.id)
-		const embed = new MessageEmbed()
-			.setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
-			.addFields(
-				{
-					name: `${user.tag}`,
-					value: `<@${user.id}>`,
-				},
-				{
-					name: 'Joined server',
-					value: `${new Date(member.joinedTimestamp).toLocaleDateString()} ${new Date(member.joinedTimestamp).toLocaleTimeString()}`,
-				},
-				{
-					name: 'Account created',
-					value: `${new Date(user.createdTimestamp).toLocaleDateString()} ${new Date(user.createdTimestamp).toLocaleTimeString()}`,
-				}
-			)
-			.setTimestamp(interaction.createdAt)
-			.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
-		if (user) return interaction.reply({embeds: [embed]});
-		return interaction.reply({embeds: [embed]});
+		fetch(api + user.id, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+			const embed = new MessageEmbed()
+				.setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
+				.addFields(
+					{
+						name: `${user.tag}`,
+						value: `ID: \`${data.id}\``,
+					},
+					{
+						name: 'Joined server',
+						value: `${new Date(member.joinedTimestamp).toLocaleDateString()} ${new Date(member.joinedTimestamp).toLocaleTimeString()}`,
+					},
+					{
+						name: 'Account created',
+						value: `${new Date(user.createdTimestamp).toLocaleDateString()} ${new Date(user.createdTimestamp).toLocaleTimeString()}`,
+					}
+				)
+				.setTimestamp(interaction.createdAt)
+				.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+			if (user) return interaction.reply({embeds: [embed]});
+			return interaction.reply({embeds: [embed]});
+		})
 	},
 };
