cc507e8eb390314651f0140e408b534fa8b2aba7
l3nnartt <loesche.lennart@gmail.com>
2021-09-13 01:57:23 +0200

    Add Webhook
    Add Webhook to ping lennart if the bot got kicked

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const { webhook } = require('../config.json');
+var request = require('request');
+
 module.exports = {
 	name: 'ready',
 	once: true,
@@ -5,12 +8,31 @@ module.exports = {
 		//Start Log
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 
-		//RPC
+		//Alle 15 Sekunden
 		setInterval(() => {
+			//RPC
 			let activity = `${client.guilds.cache.reduce((a, g) => a + g.memberCount,0)} Members`;
 			client.user.setActivity(activity,
 				{ type: "PLAYING" }
 			);
+
+			//Checks if the Bot is on a Server
+			var members = client.guilds.cache.reduce((a, g) => a + g.memberCount,0);
+			console.log(parseInt(members))
+			if (parseInt(members) == 0) {
+				var options = {
+					'method': 'POST',
+					'url': webhook,
+					formData: {
+						'payload_json': '{"content": "<@398101340322136075> Alarm!","embeds": [{"title": "STOP","description": "Der Bot wurde gekickt, das Projekt ist beendet!","color": 16711680}]}'
+					}
+				};
+
+				request(options, function (error, response) {
+					if (error) throw new Error(error);
+					console.log(response.body);
+				});
+			}
   		},15000);
 	},
 };
